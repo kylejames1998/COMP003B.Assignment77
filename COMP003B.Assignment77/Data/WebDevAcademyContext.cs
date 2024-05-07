7554094845e6a06518c6feae9cbf2efebbf3f0a5
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-05-07 07:57:53 -0700

    set up DB context

---
+++ COMP003B.Assignment77/Data/WebDevAcademyContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.Assignment77.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assignment77.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+    }
+}
--- COMP003B.Assignment77/Program.cs
+++ COMP003B.Assignment77/Program.cs
@@ -1,3 +1,5 @@
+using COMP003B.Assignment77.Data;
+using Microsoft.EntityFrameworkCore;
 namespace COMP003B.Assignment77
 {
     public class Program
@@ -8,6 +10,10 @@ namespace COMP003B.Assignment77
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // Set up the database context.
+            builder.Services.AddDbContext<WebDevAcademyContext>(options =>
+                options.UseSqlServer("Name=ConnectionStrings:DefaultConnection"));
 
             var app = builder.Build();
 
